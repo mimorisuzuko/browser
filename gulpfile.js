5d1afcf43af0903d6995c8069d3f39024664531d
Hiroki Usuba <hrky0206+gitlab@gmail.com>
2016-08-15 00:48:28 +0900

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,58 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
-const browserify = require('gulp-browserify');
 const pug = require('gulp-pug');
 const path = require('path');
 const autoprefixer = require('gulp-autoprefixer');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const webserver = require('gulp-webserver');
 
 const src = 'src';
-const dst = 'dst';
+const dst = path.join('dst/');
 
 gulp.task('sass', () => {
 	gulp.src(path.join(src, '*.scss'))
 		.pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
 		.pipe(sass.sync())
 		.pipe(autoprefixer())
-		.pipe(gulp.dest(path.join(dst, '/')));
+		.pipe(gulp.dest(dst));
 });
 
-gulp.task('scripts', () => {
-	gulp.src(path.join(src, 'index.js'))
-		.pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
-		.pipe(browserify({
-			insertGlobals: true,
-			debug: !gulp.env.production
-		}))
-		.pipe(gulp.dest(path.join(dst, '/')));
+gulp.task('script', () => {
+	browserify(path.join(src, 'index.js'), { debug: true })
+		.transform(babelify, { presets: ['es2015'] })
+		.bundle()
+		.on('error', (err) => console.log(`Error : ${err.message}`))
+		.pipe(source('index.js'))
+		.pipe(gulp.dest(dst));
 });
 
 gulp.task('pug', () => {
 	gulp.src(path.join(src, '*.pug'))
 		.pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
 		.pipe(pug({}))
-		.pipe(gulp.dest(path.join(dst, '/')));
+		.pipe(gulp.dest(dst));
+});
+
+gulp.task('webserver', () => {
+	gulp.src(dst)
+        .pipe(webserver({
+            host: 'localhost',
+            port: 6280,
+            livereload: true
+        }));
 });
 
 gulp.task('watch', () => {
+	gulp.run('webserver');
 	gulp.run('sass');
-	gulp.run('scripts');
+	gulp.run('script');
 	gulp.run('pug');
 	gulp.watch(path.join(src, '*.scss'), ['sass']);
-	gulp.watch(path.join(src, 'index.js'), ['scripts']);
+	gulp.watch(path.join(src, '*.js'), ['script']);
 	gulp.watch(path.join(src, '*.pug'), ['pug']);
 });
 
-gulp.task('default', ['sass', 'scripts', 'pug']);
+gulp.task('default', ['sass', 'script', 'pug']);
