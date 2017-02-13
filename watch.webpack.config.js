7b528b6b5b940e091a30f11ad6a3e6f67f9b0ecc
mimorisuzuko <hrky0206@gmail.com>
2017-02-13 17:58:54 +0900

    Fixed watch

--- watch.webpack.config.js
+++ watch.webpack.config.js
@@ -1,16 +1,24 @@
 const webpack = require('webpack');
 const libpath = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const {DefinePlugin, optimize: {UglifyJsPlugin}} = webpack;
+const {DefinePlugin, optimize: {UglifyJsPlugin}, HotModuleReplacementPlugin} = webpack;
 const dst = 'docs';
 
 module.exports = {
 	entry: [
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
 		libpath.join(__dirname, 'src/')
 	],
 	output: {
 		path: libpath.join(__dirname, dst),
 		filename: 'index.js'
+	},
+	devServer: {
+		hot: true,
+		contentBase: 'docs',
+		port: 3000,
+		inline: true
 	},
 	module: {
 		loaders: [
@@ -18,12 +26,14 @@ module.exports = {
 				test: /\.jsx$/,
 				exclude: /node_modules/,
 				loaders: [
+					'react-hot-loader',
 					'babel-loader?presets[]=es2015,presets[]=react'
 				]
 			},
 			{
 				test: /\.scss$/,
 				use: [
+					'react-hot-loader',
 					'style-loader',
 					'css-loader',
 					'postcss-loader',
@@ -36,6 +46,7 @@ module.exports = {
 		extensions: ['.js', '.jsx']
 	},
 	plugins: [
+		new HotModuleReplacementPlugin(),
 		new CleanWebpackPlugin([dst], {
 			root: __dirname,
 			verbose: false,
