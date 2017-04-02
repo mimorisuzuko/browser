058afb809ba137e1ccacd384fd3c2eb261c09d36
mimorisuzuko <hrky0206@gmail.com>
2017-04-02 15:03:03 +0900

    Updated

--- watch.webpack.config.js
+++ watch.webpack.config.js
@@ -1,21 +1,10 @@
-const libpath = require('path');
-const { HotModuleReplacementPlugin } = require('webpack');
-
 const config = Object.assign({}, require('./webpack.config'), {
-	entry: [
-		'webpack-dev-server/client?http://0.0.0.0:3000',
-		'webpack/hot/only-dev-server',
-		libpath.join(__dirname, 'src/')
-	],
 	devServer: {
-		hot: true,
 		contentBase: 'docs',
 		port: 3000,
 		host: '0.0.0.0',
 		inline: true
 	}
 });
-
-config.plugins.push(new HotModuleReplacementPlugin());
 
 module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const libpath = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const {DefinePlugin, optimize: {UglifyJsPlugin}} = webpack;
+const { optimize: { UglifyJsPlugin } } = webpack;
 const dst = 'docs';
 
 module.exports = {
@@ -15,38 +14,18 @@ module.exports = {
 	module: {
 		loaders: [
 			{
-				test: /\.(js|jsx)$/,
+				test: /\.js$/,
 				exclude: /node_modules/,
 				loaders: [
-					'babel-loader?presets[]=es2015,presets[]=react'
-				]
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'postcss-loader',
-					'sass-loader'
+					'babel-loader?presets[]=es2015'
 				]
 			}
 		]
 	},
 	resolve: {
-		extensions: ['.js', '.jsx']
+		extensions: ['.js']
 	},
 	plugins: [
-		new CleanWebpackPlugin([dst], {
-			root: __dirname,
-			verbose: false,
-			dry: false,
-			exclude: ['index.html']
-		}),
-		new DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production')
-			}
-		}),
 		new UglifyJsPlugin({
 			compress: {
 				warnings: false
