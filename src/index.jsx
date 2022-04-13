81f67069c6833f863e158b41bc5cd51b885932b7
mimorisuzuko <hrky0206@gmail.com>
2022-04-13 12:28:13 +0900

    Fixed

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import App from './components/App';
 
-const { default: App } = require('./components/App');
 const root = createRoot(document.querySelector('main'));
 
 root.render(<App />);
